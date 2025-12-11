71644510a6b067300cdfdab054ec744c014e19f7
Julia Kondratsiuk <kondrlias@gmail.com>
2025-12-11 19:47:22 +0300

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Cheat-sheet/',
   plugins: [react(), tailwindcss(),],
   
 })
